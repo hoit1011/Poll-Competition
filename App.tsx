3603541500672fe7110e232027a2fed23e2d2ef7
hoit1011 <parksm071011@gmail.com>
2023-09-03 14:25:33 +0900

    second commit

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import { ScrollView, View, Text, StyleSheet, ImageBackground } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, View, Text, StyleSheet, ImageBackground, Image , TouchableOpacity} from 'react-native';
 
 const App = () => {
+  const [zoomState, setZoomState] = useState<{ img1: boolean; img2: boolean; img3: boolean; img4: boolean; img5:boolean  }>({img1: false, img2: false, img3: false, img4: false,img5: false});
+
+  const toggleZoom =(img: string) => {
+    setZoomState({ img1: false, img2: false, img3: false, img4: false, img5: false, [img]: true });
+  }
+
+  let count = 50
+  
   return (
     <View>
       <ImageBackground source={require("./picture/back.png")} style ={styles.background}>
@@ -9,10 +17,42 @@ const App = () => {
           style={styles.container}>
           <View style={styles.topheader}>
             <Text>기본 설명</Text>
-          </View> 
+          </View>
           <View style = {styles.questionView}>
               <Text style={styles.question}>1. 질문</Text>
           </View>
+          <View style={styles.cheakView}>
+            <TouchableOpacity onPress={() => toggleZoom('img1')}>
+            {zoomState.img1 ? 
+                  <Text style={[styles.cheak,zoomState.img1 && styles.zoomed]}>◉</Text> : 
+                  <Text style={styles.cheak}>◎</Text>
+            }    
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => toggleZoom('img2')}>
+            {zoomState.img2 ? 
+                  <Text style={[styles.cheak,zoomState.img2 && styles.zoomed]}>◉</Text> : 
+                  <Text style={styles.cheak}>◎</Text>
+            }    
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => toggleZoom('img3')}>
+            {zoomState.img3 ? 
+                  <Text style={[styles.cheak,zoomState.img3 && styles.zoomed]}>◉</Text> : 
+                  <Text style={styles.cheak}>◎</Text>
+            }    
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => toggleZoom('img4')}>
+            {zoomState.img4 ? 
+                  <Text style={[styles.cheak,zoomState.img4 && styles.zoomed]}>◉</Text> : 
+                  <Text style={styles.cheak}>◎</Text>
+            }    
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => toggleZoom('img5')}>
+            {zoomState.img5 ? 
+                  <Text style={[styles.cheak,zoomState.img5 && styles.zoomed]}>◉</Text> : 
+                  <Text style={styles.cheak}>◎</Text>
+            }    
+            </TouchableOpacity>
+            </View>
         </ScrollView>
       </ImageBackground>
     </View>
@@ -26,6 +66,9 @@ const styles = StyleSheet.create({
   background: {
     width:"100%",
     height:"100%",
+  },
+  arrow:{
+    fontSize:112,
   },
   item: {
     marginVertical:10,
@@ -42,6 +85,7 @@ const styles = StyleSheet.create({
  },
  question: {
    textAlign:"center",
+   fontWeight:"bold",
  },
  questionView:{
   alignSelf:"center", 
@@ -52,7 +96,16 @@ const styles = StyleSheet.create({
    backgroundColor:"skyblue",
    borderRadius:25,
    justifyContent:"center",
- }
+ },
+ cheak:{
+  fontSize:60,
+ },
+ cheakView:{
+  flexDirection:"row",
+ },
+ zoomed:{
+  fontSize:55,
+ },
 });
 
 export default App;
